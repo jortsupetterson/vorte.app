00aa71740d31a643896c84d5af67a68fedb5df73
Kehityspaallikko <system@vorte.org>
2025-06-17 00:46:10 +0300

    backup

--- dashboard/src/edge/view/renderView.js
+++ dashboard/src/edge/view/renderView.js
@@ -7,6 +7,7 @@ export default function renderView(lang, url) {
     <html lang="fi" data-theme="dark" data-contrast="normal">
     <head>
     <meta charset="UTF-8">
+    <meta name="robots" content="noindex, follow">
     <link rel="stylesheet" href="/dash/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1, user-scalable=no">
     <title>Document</title>
